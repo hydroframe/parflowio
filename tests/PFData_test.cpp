05574ba3e04879836ba23b352f50ae27780cf003
Catherine Olschanowsky <catherineolschan@boisestate.edu>
2020-07-24 13:33:24 -0600

    Adding in some broken tests

--- tests/PFData_test.cpp
+++ tests/PFData_test.cpp
@@ -21,3 +21,29 @@ TEST(Test1, loadFile1){
   EXPECT_EQ(0,retval);
 }
 
+TEST(Test1, data1){
+    // this file does not exist, therefore, load should fail
+    PFData test("badname");
+    int retval = test.loadFile();
+    EXPECT_NE(0,retval);
+}
+
+TEST(Test1, data2){
+    // this file needs to exist, and should load
+    PFData test("tests/var/validfilename.pdb");
+    int retval = test.loadFile();
+    EXPECT_EQ(0,retval);
+}
+
+TEST(Test1, header1){
+    // Let's make sure that we can load the header information from the file
+    PFData test("tests/var/validfilename.pdb");
+    int retval = test.loadFile();
+    int NX = test.get_NX();
+    int NY= test.getNY();
+    int NZ = test.getNZ();
+    EXPECT_EQ(41,NX);
+    EXPECT_EQ(41,NY);
+    EXPECT_EQ(5,NZ);
+}
+

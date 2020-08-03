118e553dc1ab288fc4a4123e69c8061a574927c5
Will Lawrence <willlawrence1@u.boisestate.edu>
2020-08-03 17:04:55 -0600

    the changes I made

--- tests/PFData_test.cpp
+++ tests/PFData_test.cpp
@@ -3,7 +3,9 @@
 //
 #include "gtest/gtest.h"
 #include "parflow/pfdata.hpp"
-
+#include <iostream>
+#include <fstream>
+#include <string>
 class PFData_test : public ::testing::Test {
 protected:
 virtual void SetUp()
@@ -18,7 +20,7 @@ virtual void TearDown() {
 TEST(Test1, loadFile1){
   PFData test;
   int retval = test.loadFile();
-  EXPECT_EQ(0,retval);
+  EXPECT_NE(0,retval);
 }
 
 TEST(Test1, data1){
@@ -30,13 +32,14 @@ TEST(Test1, data1){
 
 TEST(Test1, data2){
     // this file needs to exist, and should load
-    PFData test("tests/var/validfilename.pdb");
+    PFData test("tests/var/press.init.pdb");
     int retval = test.loadFile();
     EXPECT_EQ(0,retval);
 }
 
-TEST(Test1, header1){
+//TEST(Test1, header1){
     // Let's make sure that we can load the header information from the file
+<<<<<<< HEAD
     PFData test("tests/var/validfilename.pdb");
     int retval = test.loadFile();
     int NX = test.getNX();
@@ -45,5 +48,41 @@ TEST(Test1, header1){
     EXPECT_EQ(41,NX);
     EXPECT_EQ(41,NY);
     EXPECT_EQ(5,NZ);
+=======
+//    PFData test("tests/var/validfilename.pdb");
+  //  int retval = test.loadFile();
+   // int NX = test.get_NX();
+   // int NY= test.getNY();
+   // int NZ = test.getNZ();
+   // EXPECT_EQ(41,NX);
+   // EXPECT_EQ(41,NY);
+   // EXPECT_EQ(5,NZ);
+//}
+TEST(Test1, emptyFile){
+	std::ofstream MyFile("emptyFile");
+	MyFile.close();
+	PFData test("emptyFile");
+	int retval = test.loadFile();
+	EXPECT_EQ(0, retval);
+	
+>>>>>>> the changes I made
 }
 
+TEST(Test1, readFile){
+	std::ofstream readFile("newFile");
+	std::string  data="";
+	std::string test="this will show up in the file.\n";
+	
+	readFile << test;
+	readFile.close();
+	std::ifstream infile; 
+    infile.open("newFile");
+	//infile.getline(data,100);
+	while(!infile.eof()){
+			infile >> data;}
+	//std::cout<<data<<std::endl;
+	EXPECT_EQ(0,data.compare(test));
+
+	
+	
+}

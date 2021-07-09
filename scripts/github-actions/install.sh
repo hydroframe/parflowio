13e343d7ee93d9df3ea35ad509ca3256d341e41a
cathieO <cathieO@users.noreply.github.com>
2021-07-09 10:41:56 -0600

    Update install.sh
    attempting to fix build on windows. forcing version of swig.

--- scripts/github-actions/install.sh
+++ scripts/github-actions/install.sh
@@ -4,7 +4,7 @@ set -ev
 pip install cibuildwheel==1.11.0
 
 if [[ $RUNNER_OS == "Windows" ]]; then
-  choco install -y swig --version=3.0.12
+  choco install -y swig --version=3.0.12 --force
   pip install wheel numpy==1.18.5
 elif [[ $RUNNER_OS == "macOS" ]]; then
   brew install swig
